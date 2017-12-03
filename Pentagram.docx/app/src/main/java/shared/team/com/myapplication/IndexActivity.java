2262d6f9eeec20333e18b79816b54feb06c46a86
cx49085 <1140746132@qq.com>
2017-12-03 23:20:19 +0800

    上传图片以及android
    git commit -m上传图片以及android

---
+++ Pentagram.docx/app/src/main/java/shared/team/com/myapplication/IndexActivity.java
@@ -0,0 +1,14 @@
+package shared.team.com.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Window;
+
+public class IndexActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_index);
+    }
+}
